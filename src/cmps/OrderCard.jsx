fe7c055d852d97c470256fe01ca0d717c6aa93ca
Shany Baruch <baruchshany@gmail.com>
2026-01-17 20:19:33 +0200

    Design order card in details

--- src/cmps/OrderCard.jsx
+++ src/cmps/OrderCard.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 
 
-export function OrderCard(){
+export function OrderCard() {
     const stay = useSelector(storeState => storeState.stayModule.stay)
     const navigate = useNavigate()
 
@@ -10,15 +10,20 @@ export function OrderCard(){
         <section className="order-card">
             <div className="order-header">
                 <p className="price">
-                    ₪{stay.price}/ night
+                    <span className="span1">
+                        ₪{stay.price}
+                    </span>
+                    <span className="span2">
+                        for 2 nights
+                    </span>
                 </p>
-                
-                <div className="order-rating">
+
+                {/* <div className="order-rating">
                     <span className="rate">{stay.rate}</span>
                     <span className="reviews">· {stay.reviews.length} reviews</span>
-                </div>
+                </div> */}
             </div>
-            
+
             <div className="order-summary">
                 <div className="order-dates">
                     <div className="date-box">
@@ -30,33 +35,33 @@ export function OrderCard(){
                         <span className="value">Add date</span>
                     </div>
                 </div>
-                
+
                 <div className="order-guests">
                     <span className="label">GUESTS</span>
                     <span className="value">Add guests</span>
                 </div>
             </div>
 
-           <div className="order-payment">
-            <div className="payment-row">
-                <span >₪26 × 7 nights</span>
-                <span>₪182</span>
+            {/* <div className="order-payment">
+                <div className="payment-row">
+                    <span >₪26 × 7 nights</span>
+                    <span>₪182</span>
                 </div>
-                
+
                 <div className="payment-row">
                     <span >Service fee</span>
                     <span>₪78</span>
                 </div>
 
                 <hr></hr>
-                
+
                 <div className="payment-row total">
                     <span>Total</span>
                     <span>₪260</span>
                 </div>
-            </div>
+            </div> */}
 
-            
+
             <button className="reserve-btn" onClick={() => navigate('order')}> Reserve </button>
             <p className="order-note"> You won’t be charged yet </p>
         </section>
