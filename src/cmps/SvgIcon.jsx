283654699210e6048ae27e61d7f9e583827fc623
Almog Mizrahi <almogosh111@gmail.com>
2026-01-28 20:23:19 +0200

    Add svg icon

--- src/cmps/SvgIcon.jsx
+++ src/cmps/SvgIcon.jsx
@@ -25,7 +25,14 @@ function _getIcon(iconName) {
                 <path d="M22 23h-6.001a1 1 0 0 1-1-1v-5.455a2.997 2.997 0 1 0-5.993 0V22a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V11.543a1.002 1.002 0 0 1 .31-.724l10-9.543a1.001 1.001 0 0 1 1.38 0l10 9.543a1.002 1.002 0 0 1 .31.724V22a1 1 0 0 1-1 1Z"></path>
             </svg>
         ),
-
+        
+        Arrow: (
+            <svg viewBox="0 0 32 32" role="img" height="24" width="24" fill="currentColor">
+                <title>Arrow</title>
+                <path d="M4 16h26M15 28 3.7 16.7a1 1 0 0 1 0-1.4L15 4"></path>
+            </svg>
+        ),
+        
         Search: (
             <svg aria-label="Search" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24">
                 <title>Search</title>
