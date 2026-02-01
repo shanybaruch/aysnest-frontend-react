9b3181971a2a3c48ddf59b96536e57c3db9d810f
Almog Mizrahi <almogosh111@gmail.com>
2026-02-01 17:27:42 +0200

    Add a start to steps 2 +3 inside AddStayModal

--- src/cmps/AddStayModal .jsx
+++ src/cmps/AddStayModal .jsx
@@ -43,12 +43,12 @@ export function AddStayModal({ onClose }) {
             <section className="modal">
                 <div className="add-stay-header">
                     <button className="btn-close" onClick={onClose}>✕</button>
-                    <h2>What would you like to host?</h2>
                 </div>
 
                 {/* ---------------- STEP 1 ---------------- */}
                 {step === 1 && (
-                    <section className="step">
+                    <section className="step1">
+                        <h2>What would you like to host?</h2>
 
                         <div className="add-stay-options">
                             <div className={`add-stay-card ${stay.type === 'Home' ? 'selected' : ''}`} onClick={() => updateStay('type', 'Home')}>
@@ -68,31 +68,46 @@ export function AddStayModal({ onClose }) {
 
                 {/* ---------------- STEP 2 ---------------- */}
                 {step === 2 && (
-                    <section className="step">
-                        <h2>Tell us about your place</h2>
+                    <section className="step2">
+                        <div className="header">
+                            <h2>Step 1 - Tell us about your place</h2>
+                            <p>
+                                Share some basic info, like where it is and how many guests can stay.
+                            </p>
+                        </div>
 
-                        <input
-                            placeholder="Stay name"
-                            value={stay.name}
+                        <div className="about">
+                            <input className="stay-name" placeholder="Stay name" value={stay.name}
                             onChange={ev => updateStay('name', ev.target.value)}
-                        />
-
-                        <input
-                            type="number"
-                            placeholder="Price per night"
-                            value={stay.price}
+                            />
+                            
+                            <input className="price-per-night" type="number" placeholder="Price per night" value={stay.price}
                             onChange={ev => updateStay('price', +ev.target.value)}
-                        />
-
-                        <input
-                            type="number"
-                            placeholder="Capacity"
-                            value={stay.capacity}
+                            />
+                            
+                            <input className="capacity" type="number" placeholder="Capacity" value={stay.capacity}
                             onChange={ev => updateStay('capacity', +ev.target.value)}
-                        />
+                            />
+
+                            <input className="capacity" type="number" placeholder="Capacity" value={stay.capacity}
+                            onChange={ev => updateStay('capacity', +ev.target.value)}
+                            />
+                            
+                            <input className="capacity" type="number" placeholder="Capacity" value={stay.capacity}
+                            onChange={ev => updateStay('capacity', +ev.target.value)}
+                            />
+                            
+                            <input className="capacity" type="number" placeholder="Capacity" value={stay.capacity}
+                            onChange={ev => updateStay('capacity', +ev.target.value)}
+                            />
+                            
+                            <input className="capacity" type="number" placeholder="Capacity" value={stay.capacity}
+                            onChange={ev => updateStay('capacity', +ev.target.value)}
+                            />
+                        </div>
 
                         <div className="actions">
-                            <button onClick={prevStep}>Back</button>
+                            <button className="btn-back" onClick={prevStep}>Back</button>
                             <button
                                 className="btn-next"
                                 disabled={!stay.name || !stay.price}
@@ -106,8 +121,13 @@ export function AddStayModal({ onClose }) {
 
                 {/* ---------------- STEP 3 ---------------- */}
                 {step === 3 && (
-                    <section className="step">
-                        <h2>Amenities</h2>
+                    <section className="step3">
+                        <div className="header" >
+                            <h2>Step 2 - Make it stand out</h2>
+                            <p>
+                                Add 5 or more photos plus a title and description—we’ll help you out.
+                            </p>
+                        </div>
 
                         <div className="amenities">
                             {['Wifi', 'Kitchen', 'Pool', 'TV', 'AirConditioning'].map(amenity => (
