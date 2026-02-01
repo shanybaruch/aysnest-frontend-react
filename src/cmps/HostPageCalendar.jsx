7fee899ef0d9b2cdb2dbfd4b9bf725f964c98296
Almog Mizrahi <almogosh111@gmail.com>
2026-02-01 21:40:43 +0200

    change css for host pages

--- src/cmps/HostPageCalendar.jsx
+++ src/cmps/HostPageCalendar.jsx
@@ -1,8 +1,10 @@
 export function HostPageCalendar() {
     return (
-        <section className="host-header-page">
-            <h2>Host Calendar Page</h2>
-            <p>This is the host calendar page content.</p>
+        <section className="host-page-calendar">
+            <div className="host-calender-header">
+                <h2>Host Calendar Page</h2>
+                <p>This is the host calendar page content.</p>
+            </div>
         </section>
     )
 }
